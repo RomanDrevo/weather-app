fb509a554d1c93de0a36ce2485f121d173499ead
Roman <romulus.hund@gmail.com>
2020-08-07 16:53:40 +0300

    navbar been created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,103 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import PageLayout from './components/PageLayout';
 import style from './css/App.scss';
 import {connect} from 'react-redux';
 import {fetchItems} from './store/actions/registerActions';
-import { createMuiTheme, Container, ThemeProvider } from "@material-ui/core";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import CssBaseline from "@material-ui/core/CssBaseline";
-
-
+import { createMuiTheme, Container, ThemeProvider } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from "./components/Navbar";
 
 const App = ({fetchItems}) => {
 
-    useEffect(() => {
-        fetchItems();
-    }, []);
+  const [city, setCity] = useState('Jerusalem');
+  const [error, setError] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [forecast, setForecast] = useState([]);
 
-  return (
-      <div className={style['app-wrapper']}>
-        <PageLayout>
-          <div className="main-page-content">
-            Main
-          </div>
-        </PageLayout>
+  useEffect(() => {
+    getWeather(city)
+      .then(weather => {
+        setCurrentWeather(weather);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
+  }, [city, error]);
+
+  useEffect(() => {
+    getForecast(city)
+      .then(data => {
+        setForecast(data);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
+  }, [city, error]);
+
+  const handleCityChange = city => {
+    setCity(city);
+  };
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Inter',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"'
+      ].join(','),
+      fontSize: 14,
+      h5: {
+        fontWeight: 600
+      }
+    }
+  });
+
+  if (
+    (currentWeather && Object.keys(currentWeather).length) ||
+    (forecast && Object.keys(forecast).length)
+  ) {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <Container maxWidth="sm">
+          <Weather
+            city={city}
+            currentWeather={currentWeather}
+            forecast={forecast}
+            onCityChange={handleCityChange}
+            error={error}
+          />
+        </Container>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <div>
+        <CircularProgress color={error ? 'secondary' : 'primary'} />
+        {error ? <p>{error}</p> : ''}
       </div>
-  );
+    );
+  }
+
+  // return (
+  //     <div className={style['app-wrapper']}>
+  //       <PageLayout>
+  //         <div className="main-page-content">
+  //           Main
+  //         </div>
+  //       </PageLayout>
+  //     </div>
+  // );
 };
 
 const mapStateToProps = state => {
